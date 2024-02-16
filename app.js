538f498c0df0e37c9feb55d297d962092d3d9822
Annie Treasa Sabu <anskpally@gmail.com>
2024-02-16 21:18:06 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import user from './src/routes/user'
-import { engine } from 'express-handlebars';
+const authRoutes = require('./src/routes/authRoutes');
+const cookieParser = require('cookie-parser');
+//import { engine } from 'express-handlebars';
 
 require('dotenv').config()
 //const handlebars = require('express-handlebars');
@@ -8,15 +10,24 @@ require('dotenv').config()
 const app = express()
 
 // Configure Handlebars view engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views'); // Set the path to Handlebars templates
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.set('views', './views'); // Set the path to Handlebars templates
 
+// view engine
+app.set('view engine', 'ejs');
+
+// middleware
+app.use(express.static('public'));
 app.use(express.json())
+app.use(authRoutes)
+app.use(cookieParser());
+
+
 app.use(express.urlencoded({extended:true}))
 app.use('/',user)
 // app.get('/', (req, res) => {
-//     res.render('home');
+//     res.render('trav_home');
 // });
 
 app.listen(process.env.PORT)
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const {Router} =  require('express');
+const authController = require('../controllers/authController');   
+
+const router = Router();
+
+router.get( '/trav_signup', authController.signup_get);
+router.post( '/trav_signup',authController.signup_post);
+router.get( '/trav_login', authController.login_get);
+router.post( '/trav_login', authController.login_post);
+
+module.exports = router;
