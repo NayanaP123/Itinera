b94945cd05645ced1e936e2e766869ce167b05d2
Annie Treasa Sabu <anskpally@gmail.com>
2024-04-04 13:46:04 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,9 @@
 const {Router} =  require('express');
-//const authController = require('../controllers/authController');   
-//const neo4j = require('../models/user'); // Import user model
 import userModel from '../models/user'
 const router = Router();
 
 
 
-//router.get( '/login', authController.trav_login_get);
-//router.post( '/login', authController.trav_login_post);
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
   
@@ -47,9 +43,6 @@ router.get( '/logout', async (req, res) => {
     return res.render('home');
 });
 
-// // traveller
-// router.get( '/trav_signup', authController.trav_signup_get);
-// router.post( '/trav_signup',authController.trav_signup_post);
 
 
 module.exports = router;
